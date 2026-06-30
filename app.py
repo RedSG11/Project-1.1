894a62113e7f2f8f00cc36f4b83d57ccc5ff5c99
RedSG11 <nguyenhuy51thd@gmail.com>
2026-06-30 12:18:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,9 @@ st.subheader("Subheader")
 st.text("Normal text")
 st.markdown("**Bold** and *Italic*")
 st.latex(r"P(A | B) = \frac{P(B | A) * P(A)}{P(B)}")
-st.write("write?")
+st.write("write")
+st.code("def average(list): return sum(list)/len(list)", language = "python")
+with st.echo():
+  def average(list): 
+    return sum(list)/len(list)
+  print(average([1, 2, 3, 4]))

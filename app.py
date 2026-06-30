1972e220be35d209e7b2118f3e6650071a05e6f4
RedSG11 <nguyenhuy51thd@gmail.com>
2026-06-30 12:15:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-import matplotlib.pyploy as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
 import io 
 from PIL import Image 
+st.title("Title")
+st.header("Header")
+st.subheader("Subheader")
+st.text("Normal text")
+st.markdown("**Bold** and *Italic*")
+st.latex(r"P(A | B) = \frac{P(B | A) * P(A)}{P(B)}")
+st.write("write?")

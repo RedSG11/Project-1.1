9e8c6e0f9e5b072e65823360377e61a4f1f5397a
RedSG11 <nguyenhuy51thd@gmail.com>
2026-06-30 15:22:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -45,3 +45,11 @@ if f is not None:
   content = f.read()
   st.success("Uploaded")
   st.write(content)
+if "messages" not in st.session_state:
+  st.session_state.messages = []
+prompt = st.chat_input("Ask")
+if prompt: 
+  st.session_state.messages.append({"role" : "user", "content" : prompt})
+for m in st.session_state.messages:
+  with st.chat_message(m["role"]):
+    st.write(m["content"])
